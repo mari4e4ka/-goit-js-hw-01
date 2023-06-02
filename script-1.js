674a014d146c9d2193c8e9b6f464f97c55caf619
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-06-02 20:17:55 +0300

    js - the first steps

---
+++ script-1.js
@@ -0,0 +1,116 @@
+const x1 = 10;
+
+const x2 = 30;
+
+const number = 35;
+
+const res = number > x1 && number < x2;
+
+
+// if(res === true) {
+// console.log(`Число ${number} лежить в проміжку від ${x1} до ${x2}.`);    
+// } else {
+//     console.log(`Число ${number} не лежить в проміжку від ${x1} до ${x2}.`);  
+// }
+
+const message = res === true ? `Число ${number} лежить в проміжку від ${x1} до ${x2}.` : `Число ${number} не лежить в проміжку від ${x1} до ${x2}.`;
+
+console.log(message);
+
+
+
+
+
+
+
+
+// const age = "55";
+
+// let totalPrice = "200.78";
+// totalPrice = "200.78";
+
+// const userName = "Mary";
+
+// const message = "Вітаємо!";
+
+// const isOpen = true;
+
+// const shouldConfirm = false;
+
+// console.log('Імя:', userName);
+// console.log('Вік:', age);
+// console.log(message);
+// console.log('Реєстрація:', isOpen);
+// console.log('Підтвердження:', shouldConfirm);
+// console.log('Вартість проживання в готелі:', totalPrice,'грн');
+
+// const type = typeof totalPrice;
+
+// console.log(type);
+
+// // alert('Введіть своє імя');
+
+// // const shouldRenew = confirm('Чи хочете продовжити підписку?');
+
+// // let quantity = prompt ('Вкажіть кількість днів перебування у готелі');
+// // quantity = Number(quantity);
+// // console.log('Кількість днів:', quantity );
+// // console.log(typeof quantity);
+
+// let elementWidth = '250.78998766';
+
+// // elementWidth = Number.parseInt(elementWidth);
+
+// // elementWidth = elementWidth.toFixed(2);
+
+// console.log(elementWidth);
+// console.log(typeof elementWidth);
+
+// console.log(Number(message));
+
+// console.log(Math);
+// const resulte = Math.pow(2,5);
+// console.log(resulte);
+
+// // const base = 2;
+// // const power = 3;
+// // const result = base / power;
+// // console.log(result.toFixed(2));
+
+
+// // const base = 2;
+// // const power = 3;
+// // const result = Math.pow(base,power);
+// // console.log(result);
+// console.log(2 ** 5);
+
+
+// // console.log(parseInt(Math.random() * (50 - 1) + 1));
+
+// const max = 50;
+// const min = 1;
+// const res = Math.round(Math.random() * (max - min) + min);
+// console.log(res);
+
+
+// const colors = ['red', 'teal', 'green'];
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.rundom() * (max - min) + min);
+
+// // const color = color[index];
+// // console.log(color);
+// // document.body.style.backgroundColor = color;
+
+// console.log(index);
+
+
+
+
+
+
+
+
+
+
